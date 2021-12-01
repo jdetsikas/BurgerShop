9c2140d68df73c4ce4f5fceae673f9132d1711a1
A-Mael <Crescence.Amvouna@outlook.fr>
2021-12-01 12:22:46 -0500

    V#1-01 fix-bug

--- BurgerShop.py
+++ BurgerShop.py
@@ -101,7 +101,7 @@ def user_input_drink():
     #drink_order = input("Which drink would you like? ")
     #d = Drink(drink_order, drink_dict[drink_order])
 
-    #return d
+    return d
 
 
 def user_input_side():
@@ -139,10 +139,9 @@ menu = {
       
         "The Triple Bypass" : Burger("The Triple Bypass", 22.00, "Brioche", "Angus Patty", "Cheddar", ["grilled onions", "bacon"], \
             "Six All-American Angus patties topped with cheddar, grilled onions and a mountain of bacon!!! So good that you won't \
-                even regret saying goodbye to your family!")
-        "The classic ": Burger("The classic ", 12.00, "Brioche", "beef Patty", "Cheddar",
-                                    ["lettuce", "onions","tomatoes" ,"mayo", "bacon"],
-                                    "made with 100% pure beef topped with lettuce, onions, tomatoes and cheddar.")
+                even regret saying goodbye to your family!"),
+        "The Classic": Burger("The classic ", 12.00, "Brioche", "beef Patty", "Cheddar",["lettuce", "onions","tomatoes" ,"mayo", "bacon"],\
+                              "Made with 100% pure beef topped with lettuce, onions, tomatoes and cheddar.")
     },
 
     "Sides" : {
